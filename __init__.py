2dbfce22218204ed60e984dc55f4448bde822605
AIFSH <1509359472@qq.com>
2024-10-17 08:08:27 +0800

    make low ram runable

--- __init__.py
+++ __init__.py
@@ -18,7 +18,8 @@ import numpy as np
 from PIL import Image
 from pathlib import Path
 from omegaconf import OmegaConf
-from inference_long import inference_process
+from .inference import inference_process
+# from inference_long import inference_process
 from hallo.utils.util import merge_videos
 
 class Hallo2Node:
@@ -31,21 +32,15 @@ class Hallo2Node:
             "required":{
                 "source_image":("IMAGE",),
                 "driving_audio":("AUDIO",),
-                "if_upscale":("BOOLEAN",{
+                "inference_steps":("INT",{
+                    "default": 40
+                }),
+                "use_cut":("BOOLEAN",{
                     "default": False,
                 }),
-                "pose_weight":("FLOAT",{
-                    "default":1.0
-                }),
-                "face_weight":("FLOAT",{
-                    "default":1.0
-                }),
-                "lip_weight":("FLOAT",{
-                    "default":1.0
-                }),
-                "face_expand_ratio":("FLOAT",{
-                    "default":1.2
-                }),
+                "seed":("INT",{
+                    "default":42
+                })
             }
         }
     
@@ -58,8 +53,8 @@ class Hallo2Node:
 
     CATEGORY = "AIFSH_hallo2"
 
-    def gen_video(self,source_image,driving_audio,if_upscale,
-                  pose_weight,face_weight,lip_weight,face_expand_ratio):
+    def gen_video(self,source_image,driving_audio,inference_steps,
+                  use_cut,seed):
         os.makedirs(save_dir,exist_ok=True)
         config = OmegaConf.load(config_file)
         source_image_path = osp.join(save_dir,"source_image.jpg")
@@ -86,10 +81,10 @@ class Hallo2Node:
         config.wav2vec.model_path = osp.join(hallo_dir,"wav2vec/wav2vec2-base-960h")
         config.audio_separator.model_path = osp.join(hallo_dir,"audio_separator/Kim_Vocal_2.onnx")
         config.vae.model_path = osp.join(hallo_dir,"sd-vae-ft-mse")
-        config.face_expand_ratio = face_expand_ratio
-        config.pose_weight = pose_weight
-        config.face_weight = face_weight
-        config.lip_weight = lip_weight
+
+        config.seed = seed
+        config.inference_steps = inference_steps
+        config.use_cut = use_cut
         save_seg_path = inference_process(config)
         out_video = osp.join(Path(save_seg_path).parent, "merge_video.mp4")
         merge_videos(save_seg_path,out_video)
@@ -97,6 +92,7 @@ class Hallo2Node:
         video_name = f"hallo2_{time_name}"
         res_video = osp.join(output_dir,f"{video_name}.mp4")
         # res_video_2x = osp.join(output_dir,f"hallo2_2x_{time_name}.mp4")
+        '''
         if if_upscale:
             result_root = osp.join(save_dir,f'hq_results/{video_name}_{0.5}_{2}')
             os.makedirs(result_root,exist_ok=True)
@@ -106,10 +102,51 @@ class Hallo2Node:
             shutil.copy(osp.join(result_root,f"{video_name}.mp4"), res_video)
         else:
             shutil.copy(out_video,res_video)
+        '''
+        shutil.copy(out_video,res_video)
         # shutil.rmtree(save_dir,ignore_errors=True)
         return (res_video,)
 
 
+class Hallo2UpscaleNode:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required":{
+                "input_path":("VIDEO",),
+                "fidelity_weight":("FLOAT",{
+                    "default":0.5,
+                    "tooltip":"Balance the quality and fidelity. Default: 0.5"
+                }),
+                "upscale":("INT",{
+                    "default":2,
+                    "tooltip":"The final upsampling scale of the image. Default: 2"
+                }),
+            }
+        }
+    RETURN_TYPES = ("VIDEO",)
+    #RETURN_NAMES = ("image_output_name",)
+
+    FUNCTION = "gen_video"
+
+    #OUTPUT_NODE = False
+
+    CATEGORY = "AIFSH_hallo2"
+
+    def gen_video(self,input_path,fidelity_weight,upscale):
+        video_name = video_name = os.path.basename(input_path)[:-4]
+        result_root = osp.join(save_dir,f'hq_results/{video_name}_{fidelity_weight}_{upscale}')
+        os.makedirs(result_root,exist_ok=True)
+        py =  sys.executable or "python"
+        cmd = f"""{py} {osp.join(now_dir,"video_sr.py")} -i {input_path} -o {result_root} -w {fidelity_weight} -s {upscale}"""
+        os.system(cmd)
+        res_video = osp.join(output_dir,f"{video_name}.mp4")
+        shutil.copy(osp.join(result_root,f"{video_name}.mp4"), res_video)
+        return (res_video,)
+
+
+
 NODE_CLASS_MAPPINGS = {
-    "Hallo2Node": Hallo2Node
+    "Hallo2Node": Hallo2Node,
+    "Hallo2UpscaleNode":Hallo2UpscaleNode,
 }
